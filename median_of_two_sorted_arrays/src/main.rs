5e7f8809fbe4483ecf70b523836fed23b7d2fe87
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-02-10 19:57:11 -0300

    find_median_sorted_arrays melhorado

--- median_of_two_sorted_arrays/src/main.rs
+++ median_of_two_sorted_arrays/src/main.rs
@@ -45,10 +45,24 @@ pub fn find_median_sorted_arrays_iter(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
     merged[length/2] as f64
 }
 
+pub fn find_median_sorted_arrays_iter_2(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    let mut merged: Vec<i32> = nums1.iter().chain(nums2.iter()).map(|x| *x).collect::<Vec<_>>();
+    merged.sort();
+
+    let length = merged.len();
+
+    if merged.len() % 2 == 0 {
+        return (merged[length / 2] + merged[(length / 2) - 1]) as f64 / 2.0;
+    }
+
+    merged[length/2] as f64
+}
+
 fn main() {
     let nums1 = vec![1,3]; 
     let nums2 = vec![2];
 
     println!("{:?}", find_median_sorted_arrays(nums1.clone(), nums2.clone()));
-    println!("{:?}", find_median_sorted_arrays_iter(nums1, nums2));
+    println!("{:?}", find_median_sorted_arrays_iter(nums1.clone(), nums2.clone()));
+    println!("{:?}", find_median_sorted_arrays_iter_2(nums1.clone(), nums2.clone()))
 }
