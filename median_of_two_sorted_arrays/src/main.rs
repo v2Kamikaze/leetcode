eaa08af10cb4416d2f8fcba9beb45af4269ea9c5
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-02-10 04:54:55 -0300

    Median of Two Sorted Arrays

---
+++ median_of_two_sorted_arrays/src/main.rs
@@ -0,0 +1,54 @@
+pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    let length = nums1.len() + nums2.len();
+    let mut merged = vec![0; length];
+    let mut i: usize = 0;
+
+    for v in nums1.iter() {
+        merged[i] = *v;
+        i = i + 1;
+    }
+
+    for v in nums2.iter() {
+        merged[i] = *v;
+        i = i + 1;
+    }
+
+    merged.sort();
+
+    if merged.len() % 2 == 0 {
+        let start_idx = length / 2;
+        let end_idx = start_idx - 1;
+        return (merged[start_idx] + merged[end_idx]) as f64 / 2.0
+    }
+
+    merged[length/2] as f64
+}
+
+pub fn find_median_sorted_arrays_iter(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    let length = nums1.len() + nums2.len();
+    let mut merged = vec![0; length];
+    let mut i: usize = 0;
+
+    nums1.iter().chain(nums2.iter()).into_iter().for_each(|x| {
+        merged[i] = *x;
+        i = i + 1;
+    });
+
+    merged.sort();
+
+    if merged.len() % 2 == 0 {
+        let start_idx = length / 2;
+        let end_idx = start_idx - 1;
+        return (merged[start_idx] + merged[end_idx]) as f64 / 2.0
+    }
+
+    merged[length/2] as f64
+}
+
+fn main() {
+    let nums1 = vec![1,3]; 
+    let nums2 = vec![2];
+
+    println!("{:?}", find_median_sorted_arrays(nums1.clone(), nums2.clone()));
+    println!("{:?}", find_median_sorted_arrays_iter(nums1, nums2));
+}
