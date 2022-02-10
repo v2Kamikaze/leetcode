f5f027bd974a6bb28b5b3feb008128dfa586a834
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-02-10 02:34:38 -0300

    two_sum

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -16,7 +16,7 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 }
 
 fn main() {
-    let input = vec![3, 2, 4];
-    let target = 6;
+    let input = vec![2,7,11,15];
+    let target = 9;
     println!("{:?}", two_sum(input, target));
 }
